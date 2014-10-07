4ab103b46315fde949b085dde947df5dd81d7fad
mmlxvi <quarterly24@gmail.com>
2014-10-06 21:01:33 -0500

    add

--- app.js
+++ app.js
@@ -2,24 +2,72 @@ $(document).ready(function() {
 
 	var questionCounter = 1;
 	var correctNum = 0;
+	var arrayNum = 0;
+
+	var changeQuestion = function() {
+		$("#paragraph").text(arrayNum[0])
+
+	}
+
+	var qOne = ["paragraph about Rauschenberg", "Jim Dine", "Gerhard Richter",
+				"Jasper Johns", "Robert Rauschenberg", 4];
 
 	$("#qNumber").text(questionCounter);
 	$("#numberCorrect").text(correctNum);
 
+
+			
 	$("#nextQ").click(function() {
+
+	if (questionCounter < 10) {
+
 		questionCounter = questionCounter + 1;
 		$("#qNumber").text(questionCounter);
-	})
+
+		switch(changeQuestion) {
+
+			case (questionCounter = 1): alert('something');
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+		}
+	}
+
+	else if (questionCounter = 11) {
+		$("#qNumber").text(questionCounter);
+		$("#nextQ").text("Play again?");
+	}
+
+	else {
+		alert("start again?");
+	}
+
+	});
 	
 
 
 
+	
+	
+	
+	
 
 
+	
 
 
 
