39cb734e2793a79f26fe4cccd4a481223d67b4b6
mmlxvi <quarterly24@gmail.com>
2014-10-09 15:33:00 -0500

    thursday afternoon

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function qArray(information, question, choice1, choice2, choice3, choice4, corre
 }
 
 	var imageLibrary = ['images/08132012_EDU_app_raushenberg_main.jpg', 'images/albasbreakfast.jpg', 'images/nightcreatures.jpg',
-						 'images/Rivera-the-arsenal.jpg', 'images/white-center.jpg', 'images/124Good-byeA2008.jpg', 'images/CRI_298734.jpg',
+						 'images/Rivera-the-arsenal.jpg', 'images/white-center.jpg', 'images/124Good-byeA2008.jpg', 'images/number-1-lavender-mist.jpg',
 						 'images/Reading_The_Letter_Picasso_1921_small.jpg', 'images/dekooningpinkangels.jpg', 'images/morrislouissquare.jpg']
 
 
@@ -27,31 +27,31 @@ function qArray(information, question, choice1, choice2, choice3, choice4, corre
 							"Who painted this?", "Jasper Johns", "Cy Twombly", "Gerhard Richter",
 							"Robert Rauschenberg", "Robert Rauschenberg", "This silkscreen painting, Retroactive I, was created by Rauschenberg in the summer of 1963, a few months before Kennedy's assassination.", "08132012_EDU_app_raushenberg_main.jpg");
 
-	var qTwo = new qArray("This painting was created as part of a collaborative series by three well-known artists in 1984: Jean Michel Basquiat, Andy Warhol, and an Italian painter whose work is prominently featured in the 1998 film adaptation of Charles Dickens' Great Expectations starring Robert DeNiro and Gwyneth Paltrow.", "Who is the third artist?", 
-				"David Salle", "Fabrizio Plessi", "Francesco Clemente", "Roy Lichtenstein", "Francesco Clemente", "The third artist was Clemente.", "albasbreakfast.jpg");
+	var qTwo = new qArray("This piece, titled 'Bananas', was one in a series of collaborative paintings by Andy Warhol and this neo-expressionist painter who first gained notoriety for his graffiti in the Lower East Side of Manhattan.", "Who is the other painter?", 
+				"Philip Guston", "David Salle", "Jean-Michel Basquiat", "Francesco Clemente", "Jean-Michel Basquiat", "Basquiat and Warhol completed this painting in 1985. Both painters died within three years of its completion.", "bananas.jpg");
 
 	var qThree = new qArray("She was an influential American abstract expressionist painter in the second half of the 20th century, as was her husband, who was the more famous of the two. The work seen here, Night Creatures, was completed in 1965.", "Who painted this?", "Helen Frankenthaler",
-				 "Lee Krasner", "Georgia O'Keeffe", "Frida Kahlo", "Lee Krasner", "Hans Hoffman, one of Krasner's teachers, once remarked that her work was 'so good you would not know it was painted by a woman.'", 'nightcreatures.jpg');
+				 "Lee Krasner", "Georgia O'Keeffe", "Frida Kahlo", "Lee Krasner", "One of Lee Krasner's teachers once remarked that her work was 'so good you would not know it was painted by a woman.'", 'nightcreatures.jpg');
 
-	var qFour = new qArray("This Mexican painter was portrayed by Rubén Blades in the 1999 film Cradle Will Rock, and by Alfred Molina in Julie Taymor's Frida Kahlo biopic in 2002.", "Who is the painter?",
-				"José Chávez Morado", "Salvador Corona", "Fernando Leal", "Diego Rivera", "Diego Rivera", "text about Rivera", 'Rivera-the-arsenal.jpg');
+	var qFour = new qArray("This Mexican painter was portrayed by Rubén Blades in the 1999 film Cradle Will Rock, and by Alfred Molina in Julie Taymor's Frida Kahlo biopic in 2002. The painter was most famous for his murals, which prominently featured socialist themes and aspects of Mexican culture.", "Who is the painter?",
+				"José Chávez Morado", "Salvador Corona", "Fernando Leal", "Diego Rivera", "Diego Rivera", "This image is a detail from Diego Rivera's 1928 mural, The Arsenal. The central figure of the painting is Frida Kahlo, Rivera's wife.", 'Rivera-the-arsenal.jpg');
 
-	var qFive = new qArray("paragraph about Rothko", "The Rothko Chapel is located in what American city?",
-				  "Houston", "San Francisco", "Baltimore", "New Haven", "Houston", "text about chapel", 'white-center.jpg');
+	var qFive = new qArray("Mark Rothko was an Abstract Expressionist painter whose signature works are large-scale paintings of  emotionally evocative colored rectangles. In 1964, he was commissioned to create a meditative space filled with his paintings. Rothko committed suicide in 1970 and did not live to see the chapel completed.", "The Rothko Chapel is located in what American city?",
+				  "Houston", "San Francisco", "Baltimore", "New Haven", "Houston", "Composer Morton Feldman wrote a piece inspired by his visit to the chapel in Houston in 1971.", 'white-center.jpg');
 
-	var qSix = new qArray("paragraph about Salle", "Who painted this?", "Julian Schnabel", "Jeff Koons",
-				"Andy Warhol", "David Salle", "David Salle", "text about Salle", '124Good-byeA2008.jpg');
+	var qSix = new qArray("This American painter was born in 1952. He studied at CalArts and rose to fame in New York in the 1980s. His paintings often juxtapose seemingly unrelated images appropriated from popular culture and art history. In 1995 he directed the feature film Search and Destory, which starred Christopher Walken and Dennis Hopper.", "Who painted this?", "Julian Schnabel", "Jeff Koons",
+				"Andy Warhol", "David Salle", "David Salle", "This painting, 'Goodbye A.', was completed by David Salle in 2008.", '124Good-byeA2008.jpg');
 
-	var qSeven = new qArray("Known primarily for his drip paintings like the one seen here, this earlier work something something something something something. It is interesting to note that something something and how that.", "How did Jackson Pollock die?", "in a car accident",
-				   "he drowned", "in a plane crash", "heart attack", "in a car accident", "text about him", "CRI_298734.jpg");
+	var qSeven = new qArray("Jackson Pollock's early work was influenced by muralists like Diego Rivera as well as aspects of Surrealism. But Pollock is most remembered for his large 'drip-paintings' like the one seen here.", "How did Jackson Pollock die?", "in a car accident",
+				   "he drowned", "in a plane crash", "heart attack", "in a car accident", "Pollock died at the age of 44 in an alcohol-related single-car accident.", "number-1-lavender-mist.jpg");
 
-	var	qEight = new qArray("paragraph about Picasso", "Who painted this?", "George Braque", "Piet Mondrian", "Willem de Kooning",
-					"Pablo Picasso", "Pablo Picasso", "text about painting", 'Reading_The_Letter_Picasso_1921_small.jpg');
+	var	qEight = new qArray("This prolific Spanish painter is known for co-founding the Cubist movement. The painting shown here, La Lecture de la Lettre (Reading the Letter), was created circa 1921 during his transition from Cubism to Classicism.", "Who painted this?", "George Braque", "Piet Mondrian", "Willem de Kooning",
+					"Pablo Picasso", "Pablo Picasso", "This painting was found in Picasso's studio after his death.", 'Reading_The_Letter_Picasso_1921_small.jpg');
 
-	var qNine = new qArray("paragraph about Willem de Kooning", "In what year was this painting most likely completed?",
-				 "1935", "1945", "1955", "1965", "1945", "text about de Kooning", 'dekooningpinkangels.jpg');
+	var qNine = new qArray("Willem de Kooning (1904 – 1997) was a Dutch American abstract expressionist. The work shown here, 'Pink Angels', is the final painting in his first series devoted to images of women.", "In what year was this painting most likely completed?",
+				 "1935", "1945", "1955", "1965", "1945", "'Pink Angels' was completed circa 1945.", 'dekooningpinkangels.jpg');
 
-	var qTen = new qArray("paragraph about Morris Louis", "Who painted this?", "Morris Louis", "Jackson Pollock", "Mark Rothko", "Jim Dine", "Morris Louis", "text about Louis", 'morrislouissquare.jpg');
+	var qTen = new qArray("This painter's work, which provides a link between Abstract Expressionism and Color Field painting, is characterized by layered rainbows of acrylic paint poured down huge blank canvases.", "Who painted this?", "Morris Louis", "Jackson Pollock", "Mark Rothko", "Jim Dine", "Morris Louis", "Morris Louis (born Morris Louis Bernstein) created this painting circa 1959.", 'floralMorrisLouis.jpg');
 
 var metaArray = [qOne, qTwo, qThree, qFour, qFive, qSix, qSeven, qEight, qNine, qTen];
 
@@ -125,7 +125,7 @@ $('.btn').click(function(){
 			$('#feedback').text("Your score: " + score * 10 + "%");
 			$('#finalScore').text(score);
 			$('#finalScore').show();
-			$('#nextQ').remove();
+			$('#nextQ').hide();
 			$("#response").fadeIn(400);
 		}
 		$('#feedbackTxt').remove();
@@ -157,7 +157,9 @@ $('#playAgain').click(function() {
 	$('#response').fadeOut(350);
 	$('#qNumber').text(1);
 	('#painting9').hide();
-	
+	$('#playAgain').hide();
+	$('#nextQ').show();
+
 
 })
 
